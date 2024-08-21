239e11e45d47a18b1b578196bfd87f4cb579371c
Samaelish <semnd007@gmail.com>
2024-08-21 23:00:28 +0300

    frankfurter version

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,8 @@
+import CurrencyConverter from './components/CurrencyConverter'
+import './App.css'
+
+function App() {
+  return <CurrencyConverter url='https://api.frankfurter.app' />
+}
+
+export default App
